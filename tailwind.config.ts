87ca8636c0b7214a00b258f8ac07e23a7acb5482
Red Zebra <tworedzebras@gmail.com>
2025-06-24 22:28:42 +0000

    ui is not correct complete css is broken

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,11 @@
+
 import type {Config} from 'tailwindcss';
 const { fontFamily } = require("tailwindcss/defaultTheme")
 
 export default {
   darkMode: ['class'],
   content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
